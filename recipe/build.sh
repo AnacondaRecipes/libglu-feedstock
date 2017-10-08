325dfc103d6a871e6b17e5684fe065dc972d1e90
Ray Donnelly <mingw.android@gmail.com>
2017-10-08 16:08:23 +0100

    Add cross-compilation support

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export CFLAGS="-I${PREFIX}/include"
-export LDFLAGS="-L${PREFIX}/lib"
-./configure  --prefix=$PREFIX ##--disable-static
-make
+export CFLAGS="${CFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+./configure  --prefix=${PREFIX}
+make -j${CPU_COUNT} ${VERBOSE_AT}
 make install
