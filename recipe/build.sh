f16929f6be3f092975534e02cec54ccf34f7e8cc
Ray Donnelly <mingw.android@gmail.com>
2017-10-08 16:16:59 +0100

    Use cdt package for libgl

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,7 @@
 
 export CFLAGS="${CFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-./configure  --prefix=${PREFIX}
+./configure --prefix=${PREFIX} \
+            --host=${HOST}
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make install
