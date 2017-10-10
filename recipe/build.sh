1ad59cb877ca6bbcef4b1d7fdb62834f776ae9b5
Ray Donnelly <mingw.android@gmail.com>
2017-10-10 17:58:03 +0100

    Attempted (unsuccessful) fix for macOS

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,10 @@
 
 export CFLAGS="${CFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  export CPPFLAGS="${CPPFLAGS} -framework OpenGL"
+  export LDFLAGS="${LDFLAGS} -framework OpenGL"
+fi
 ./configure --prefix=${PREFIX} \
             --host=${HOST}
 make -j${CPU_COUNT} ${VERBOSE_AT}
