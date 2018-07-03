8d6b35b8de61090f2a83ae44b7ffa65c852e7658
Ray Donnelly <mingw.android@gmail.com>
2018-07-03 08:19:27 -0400

    Specify build (for ppc)

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-export CFLAGS="${CFLAGS} -I${PREFIX}/include"
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 if [[ ${HOST} =~ .*darwin.* ]]; then
   export CPPFLAGS="${CPPFLAGS} -framework OpenGL"
   export LDFLAGS="${LDFLAGS} -framework OpenGL"
 fi
 ./configure --prefix=${PREFIX} \
+            --build=${BUILD} \
             --host=${HOST}
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make install
