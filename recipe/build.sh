949fc82930299c1abeec1404ee29570052c5063f
Serhii Kupriienko <skupriienko@anaconda.com>
2025-05-02 13:21:06 +0300

    Fix libdir

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 mkdir -p build
-meson setup build ${MESON_ARGS} --prefix=${PREFIX}
+# Without ${PREFIX}/lib the library will be installed to $PREFIX/lib64.
+meson setup build ${MESON_ARGS} \
+    --prefix=${PREFIX} \
+    --libdir=${PREFIX}/lib
 
 # Build and install
 meson compile -C build
